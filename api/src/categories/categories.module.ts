04f1c76df255030ce5519b90163f109ced85217b
Rafixx <rafal987.1@o2.pl>
2024-07-12 12:40:45 +0200

    feat: add Question entity to categories module

--- api/src/categories/categories.module.ts
+++ api/src/categories/categories.module.ts
@@ -3,9 +3,10 @@ import { CategoriesService } from './categories.service';
 import { CategoriesController } from './categories.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Category } from './entities/category.entity';
+import { Question } from './entities/question.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Category])],
+  imports: [TypeOrmModule.forFeature([Category, Question])],
   controllers: [CategoriesController],
   providers: [CategoriesService],
 })
