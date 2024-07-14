aa139ead1ea9d362daf4ef0382a1b0b46613c70c
Rafixx <rafal987.1@o2.pl>
2024-07-14 13:41:29 +0200

    feat: create loading page on category details route

---
+++ client/src/app/(pages)/categories/[category]/loading.tsx
@@ -0,0 +1,26 @@
+import { PrevPageButton } from '@/components/shared/PrevPageButton';
+import { Small } from '@/components/shared/typography/Small';
+import { Skeleton } from '@/components/shared/Skeleton';
+
+export default function Loading() {
+  // You can add any UI inside Loading, including a Skeleton.
+  return (
+    <div className='flex flex-col'>
+      <PrevPageButton />
+      <Small>Category</Small>
+      <div className='flex items-center justify-between w-full h-12 mt-6'>
+        <Skeleton className='w-44 h-full' />
+        <Skeleton className='w-44 h-full' />
+      </div>
+      <div className='flex flex-col gap-4 items-center justify-center w-full h-96 overflow-hidden'>
+        <Skeleton className='w-full h-12' />
+        <Skeleton className='w-full h-12' />
+        <Skeleton className='w-full h-12' />
+        <Skeleton className='w-full h-12' />
+        <Skeleton className='w-full h-12' />
+        <Skeleton className='w-full h-12' />
+        <Skeleton className='w-full h-12' />
+      </div>
+    </div>
+  );
+}
