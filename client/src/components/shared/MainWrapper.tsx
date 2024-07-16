11cd3a1842e0883ac4b52ce7bf84716e85b48ffd
Rafixx <rafal987.1@o2.pl>
2024-07-16 08:04:10 +0200

    fix: change max width of container for larger screens

--- client/src/components/shared/MainWrapper.tsx
+++ client/src/components/shared/MainWrapper.tsx
@@ -2,7 +2,7 @@ export const MainWrapper: React.FC<{ children?: React.ReactNode }> = ({
   children,
 }) => {
   return (
-    <main className='container bg-white w-full h-full max-w-xl border py-24'>
+    <main className='container bg-white w-full h-full max-w-xl 3xl:max-w-4xl border py-24'>
       {children}
     </main>
   );
