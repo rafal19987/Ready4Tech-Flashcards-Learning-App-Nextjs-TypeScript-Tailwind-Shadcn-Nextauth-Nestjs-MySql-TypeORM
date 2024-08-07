dc563b433cf4785f25dc6b4086266d8ae3ccb3ce
Rafixx <rafal987.1@o2.pl>
2024-08-07 20:05:52 +0200

    refactor: delete unnessesery code

--- client/src/app/actions/deleteCategory.ts
+++ client/src/app/actions/deleteCategory.ts
@@ -1,11 +1,7 @@
 'use server';
 
-import { revalidatePath } from 'next/cache';
-
 export async function deleteCategory(id: number) {
   const res = await fetch(`${process.env.API_URL}/categories/${id}`, {
     method: 'DELETE',
   });
-
-  revalidatePath('/categories');
 }
